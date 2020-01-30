d3d6191a807a975a1d248b4eadc30e887cc73999
guqing <1484563614@qq.com>
2020-01-30 20:33:52 +0800

    init commit

---
+++ src/main/java/xyz/guqing/app/HaloPanelApplication.java
@@ -0,0 +1,13 @@
+package xyz.guqing.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaloPanelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaloPanelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/guqing/app/HaloPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.guqing.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaloPanelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
